9a0bb532768fabd1e2121753dcbd329e802510aa
zhongcheng <zhongcheng_m@yeah.net>
2017-12-22 09:56:01 +0800

    readme

--- src/test/java/test/syn/Lock.java
+++ src/test/java/test/syn/Lock.java
@@ -1,5 +1,7 @@
 package test.syn;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.LockSupport;
 
 /**
@@ -16,8 +18,51 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class Lock {
 
-    public static void get(){
+    public static void get() {
         LockSupport.park();
     }
 
+
+    public static void main(String[] args) {
+
+        Student student = new Student("ZHONGC",12);
+        List<Student> students = new ArrayList<>();
+
+        students.add(student);
+        student = new Student("asd",18);
+        students.add(student);
+
+    }
+
+
+    public static class Student {
+
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        private String name;
+
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public Student setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Student setAge(int age) {
+            this.age = age;
+            return this;
+        }
+    }
 }
